896906ce8ec9860b0b0c6de7d3d8ebbc451ccdcd
LB <mail@lb.ee>
2017-03-09 18:39:30 +0700

    basic responsive testing in place

--- components/grid.js
+++ components/grid.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react'
-var ResponsiveReactGridLayout = require('react-grid-layout').Responsive;
+
+import {Responsive, WidthProvider} from 'react-grid-layout';
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+import './grid.less'
 
 class DashboardGrid extends Component {
   render () {
-    // layout is an array of objects, see the demo for more complete usage
-    var layout = [
-      {i: 'logo', x: 0, y: 0, w: 2, h: 2},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2},
-      {i: 'c', x: 4, y: 0, w: 1, h: 2}
-    ];
+    let responsiveLayouts = {
+      lg: [
+        {i:'logo', w: 2, h: 2, x: 0, y: 0},
+        {i:'city', w: 3, h: 1, x: 2, y: 0},
+        {i:'clock', w: 3, h: 1, x: 2, y: 1},
+        {i:'country', w: 3, h: 4, x: 5, y: 0},
+        {i:'flights', w: 3, h: 2, x: 0, y: 3},
+        {i:'countries', w: 2, h: 2, x: 3, y: 3},
+      ]
+    };
+    let breakpoints = {lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0};
+    let cols = {lg: 16, md: 12, sm: 8, xs: 6, xxs: 2};
+
     return (
-      // <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-      <ResponsiveReactGridLayout className="layout" layout={layout}
-      breakpoints={{lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0}}
-      cols={{lg: 12, md: 10, sm: 8, xs: 4, xxs: 2}}>
-        <div key={'logo'}>LOGO</div>
-        <div key={'b'}>b</div>
-        <div key={'c'}>c</div>
+      <ResponsiveReactGridLayout
+        className="layout"
+        cols={cols}
+        rowHeight={90}
+        breakpoints={breakpoints}
+        layouts={responsiveLayouts}
+        >
+        <div key={'logo'} className={'test'}>
+          <span className="text">Logo</span>
+        </div>
+        <div key={'city'} className={'test'}>
+          <span className="text">City</span>
+        </div>
+        <div key={'clock'} className={'test'}>
+          <span className="text">Clock</span>
+        </div>
+        <div key={'country'} className={'test'}>
+          <span className="text">Country</span>
+        </div>
+        <div key={'flights'} className={'test'}>
+          <span className="text">Flights</span>
+        </div>
+        <div key={'countries'} className={'test'}>
+          <span className="text">Countries Visited</span>
+        </div>
       </ResponsiveReactGridLayout>
     )
   }
