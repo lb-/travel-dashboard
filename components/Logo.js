7e7bd77ded2834bbba3af095453a46eb5bc2688e
LB <mail@lb.ee>
2017-03-16 15:45:50 +0700

    Revise logo for dynamic url

--- components/Logo.js
+++ components/Logo.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { prefixLink } from 'gatsby-helpers';
 import './logo.less';
 
 class Logo extends Component {
   render () {
+    const logoStyle = {
+      backgroundImage: 'url(' + prefixLink('/images/logo.svg') + ')',
+    }
     return (
       <div className="contains-logo">
-        <div className="logo">
+        <div className="logo" style={logoStyle}>
           <a className="button is-small is-link" href="https://lb.ee">by lb.ee</a>
         </div>
       </div>
