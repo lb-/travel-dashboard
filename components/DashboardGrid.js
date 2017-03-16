8d4cbd3ccf84d92fd0eb235bf6309d42d2b130db
LB <mail@lb.ee>
2017-03-16 13:51:01 +0700

    re-work grid sizes

--- components/DashboardGrid.js
+++ components/DashboardGrid.js
@@ -12,20 +12,21 @@ class DashboardGrid extends Component {
   render () {
     let responsiveLayouts = {
       lg: [
-        {i:'logo', w: 2, h: 2*3, x: 0, y: 0},
-        {i:'city', w: 3, h: 1*3, x: 2, y: 0},
-        {i:'clock', w: 3, h: 1*3, x: 2, y: 1},
-        {i:'country', w: 3, h: 4*3, x: 5, y: 0},
-        {i:'flights', w: 3, h: 2*3, x: 0, y: 3},
-        {i:'stats', w: 2, h: 2*3, x: 3, y: 3},
+        {i:'logo', w: 5, h: 6, x: 0, y: 0},
+        {i:'city', w: 4, h: 3, x: 5, y: 0},
+        {i:'clock', w: 4, h: 3, x: 9, y: 0},
+        {i:'stats', w: 8, h: 3, x: 5, y: 3},
+        {i:'country', w: 7, h: 12, x: 14, y: 0},
+        {i:'flights', w: 13, h: 2*3, x: 0, y: 3},
+
       ],
       md: [
-        {i:'logo', w: 2, h: 2*2.5, x: 0, y: 0},
-        {i:'city', w: 3, h: 1*2.5, x: 2, y: 0},
-        {i:'clock', w: 3, h: 1*2.5, x: 2, y: 1},
-        {i:'country', w: 3, h: 4*2.5, x: 5, y: 0},
-        {i:'flights', w: 3, h: 2*2.5, x: 0, y: 3},
-        {i:'stats', w: 2, h: 2*2.5, x: 3, y: 3},
+        {i:'logo', w: 2, h: 6, x: 0, y: 0},
+        {i:'city', w: 2, h: 3, x: 2, y: 0},
+        {i:'clock', w: 2, h: 3, x: 4, y: 0},
+        {i:'country', w: 3, h: 11, x: 6, y: 0},
+        {i:'stats', w: 4, h: 3, x: 2, y: 2},
+        {i:'flights', w: 6, h: 5, x: 0, y: 6},
       ],
       sm: [
         {i:'logo', w: 1.5, h: 3, x: 0, y: 0},
@@ -45,21 +46,22 @@ class DashboardGrid extends Component {
       ],
       xs: [
         {i:'logo', w: 1, h: 3, x: 0, y: 0},
-        {i:'city', w: 2, h: 2, x: 3, y: 0},
-        {i:'clock', w: 2, h: 2, x: 5, y: 0},
-        {i:'country', w: 2, h: 8, x: 7, y: 0},
-        {i:'flights', w: 2, h: 6, x: 15, y: 0},
-        {i:'stats', w: 2, h: 3, x: 21, y: 0},
+        {i:'city', w: 2, h: 2.5, x: 1, y: 0},
+        {i:'clock', w: 2, h: 2.5, x: 4, y: 0},
+        {i:'stats', w: 2, h: 6, x: 0, y: 8},
+        {i:'country', w: 3, h: 7, x: 4, y: 14},
+        {i:'flights', w: 5, h: 12, x: 0, y: 21},
+
       ],
     };
     let breakpoints = {lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0};
-    let cols = {lg: 8, md: 8, sm: 6, xs: 4, xxs: 2};
+    let cols = {lg: 20, md: 9, sm: 4, xs: 5, xxs: 2};
 
     return (
       <ResponsiveReactGridLayout
         className="layout"
         cols={cols}
-        rowHeight={60}
+        rowHeight={55}
         breakpoints={breakpoints}
         layouts={responsiveLayouts}
         >
